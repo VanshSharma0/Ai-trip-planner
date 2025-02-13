6fc1e114fbd7ac7cb50cc07043149f92c5f8b036
VanshSharma0 <vanshsharma9933@gmail.com>
2025-02-13 21:57:48 +0530

    Update import paths for Navbar and PopoverBody components to include file extensions

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,8 +2,8 @@ export { default as Card } from './Card';
 export { default as Typography } from './Typography';
 export { default as Image } from './Image';
 export { default as Button } from './Button';
-export { default as Navbar } from './Navbar';
-export { default as PopoverBody } from './PopoverBody';
+export { default as Navbar } from './Navbar.jsx';
+export { default as PopoverBody } from './PopoverBody.jsx';
 
 export {
   DropdownMenu,
